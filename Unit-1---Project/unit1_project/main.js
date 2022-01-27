f0fe743df7c705871067fb02510b36895dab54c2
jasonhensley1989 <jasonhensley1989@gmail.com>
2022-01-27 15:33:58 -0800

    API final issue 2

--- Unit-1---Project/unit1_project/main.js
+++ Unit-1---Project/unit1_project/main.js
@@ -72,4 +72,11 @@ $('.other-projects').click(function() {
   });
 });
 
-//JQuery to hide nav div when hamburger button is clicked
+//
+function mymap() {
+  let mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  };
